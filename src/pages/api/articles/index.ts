d974078fec1f44ef45a3dfbf43ec3c0ee239c40c
rowaxl <rowaxl0@gmail.com>
2019-12-24 00:04:32 +0900

    update getInitialProps

--- src/pages/api/articles/index.ts
+++ src/pages/api/articles/index.ts
@@ -1,11 +1,12 @@
 import { FetchWrapper } from '../../../utils/FetchWrapper'
+import { Article } from '../../../interfaces/Article'
 
 /**
  * GET /articles
  * get all list of articles
  */
-export default async () => {
-  const res = await FetchWrapper('http://localhost:3000/articles')
+export const getArticles = async () => {
+  const res: Article[] = await FetchWrapper('http://localhost:3000/articles')
 
   return res
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,55 +2,29 @@
 import React from 'react'
 import { NextPage } from 'next'
 
+// interface
+import { Article } from '../interfaces/Article'
+
 // Components
 import Layout from '../components/Layout'
 import ArticleList from '../components/ArticleList'
 import GridContainer from '../components/Grid/GridContiner'
 import AddArticle from '../components/InputForm/AddArticle'
 
-// type DummyArticle = {
-//   id: string,
-//   title: string,
-//   impression: string,
-//   wrotenAt: number
-// }
+// API
+import { getArticles } from './api/articles'
 
-const IndexPage: NextPage = () => {
-  // TODO: GET articles/
-  const dummyArticle = [
-    {
-      "id": "01ARZ3NDEKTSV4RRFFQ69G5FAV",
-      "title": "title0",
-      "impression": "lolem ipsum",
-      "wrotenAt": 1576834054417
-    },
-    {
-      "id": "01ARZ3NDEKTSV5RRFFQ69G5FAV",
-      "title": "title2",
-      "impression": "lolem ipsum",
-      "wrotenAt": 1576834954555
-    },
-    {
-      "id": "01ARZ3NDEKTSV4RRFFQ69G5FAZ",
-      "title": "title1",
-      "impression": "lolem ipsum",
-      "wrotenAt": 1576835154555
-    },
-    {
-      "id": "01ARZ3NDEKTSFFRRFFQ69G5FAV",
-      "title": "title3",
-      "impression": "lolem ipsum",
-      "wrotenAt": 1576835354417
-    }
-  ]
+type Props = {
+  articles: Article[]
+}
 
-  // const [articles] = useState()
+const IndexPage: NextPage<Props> = ({ articles }) => {
 
   return (
     <Layout title="The Shiories">
       <h1>Home</h1>
 
-      <ArticleList items={dummyArticle} />
+      <ArticleList items={articles} />
 
       <GridContainer {...{ direction: "column", alignItems: "center" }}>
         <AddArticle />
@@ -59,8 +33,10 @@ const IndexPage: NextPage = () => {
   );
 }
 
-// IndexPage.getInitialProps = async () => {
-  
-// }
+IndexPage.getInitialProps = async () => {
+  const articles: Article[] = await getArticles()
+
+  return { articles }
+}
 
 export default IndexPage
