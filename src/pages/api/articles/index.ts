be8401a440ce23485f37d1170f19a837f3790e43
rowaxl <rowaxl0@gmail.com>
2019-12-25 18:33:41 +0900

    add post method

--- src/pages/api/articles/index.ts
+++ src/pages/api/articles/index.ts
@@ -1,4 +1,4 @@
-import { FetchWrapper } from '../../../utils/FetchWrapper'
+import fetchWrapper from '../../../utils/fetchWrapper'
 import { Article } from '../../../interfaces/Article'
 
 /**
@@ -6,7 +6,21 @@ import { Article } from '../../../interfaces/Article'
  * get all list of articles
  */
 export const getArticles = async () => {
-  const res: Article[] = await FetchWrapper('http://localhost:3000/articles')
+  const res: Article[] = await fetchWrapper('http://localhost:3000/articles')
+
+  return res
+}
+
+/**
+ * POST /articles
+ * submit new article
+ */
+export const postArticle = async (body: Article) => {
+  const res: Article = await fetchWrapper<Article>(
+    'http://localhost:3000/articles', {
+      method: 'POST',
+      body: JSON.stringify(body)
+  })
 
   return res
 }
--- src/utils/FetchWrapper.ts
+++ src/utils/FetchWrapper.ts
@@ -1,13 +1,18 @@
 import fetch from 'isomorphic-unfetch'
 
-export const FetchWrapper = async (
+const wrapper = <T>(task: Promise<Response>): Promise<T> =>
+  new Promise(
+    (resolve, reject) => task
+      .then(response => response.json())
+      .then(body => resolve(body))
+      .catch(error => reject(error))
+  )
+
+const fetchWrapper = async <T = any>(
   input: RequestInfo,
   init?: RequestInit
-) => {
-  try {
-    const data = await fetch(input, init).then(res => res.json())
-    return data
-  } catch (err) {
-    throw new Error(err.message)
-  }
+): Promise<T> => {
+  return wrapper<T>(fetch(input, init))
 }
+
+export default fetchWrapper
