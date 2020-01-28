6441e1cfd5f382103b79024dccc2573d54ae4d3e
rowaxl <rowaxl0@gmail.com>
2020-01-28 17:20:19 +0700

    add google search api

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,15 +1,13 @@
 export interface BookDetails {
   bookID: string
-  ISBN10?: string
-  ISBN13?: string
   title: string
-  publishedDate?: number
-  author?: string
+  publishedDate?: string
+  author?: Array<string>
   thumbnail?: string
   description?: string
   memo?: string
   created: number
-  [key:string]: string | number | undefined
+  [key:string]: string | number | Array<string> |undefined
 }
 
 export interface BookmarkDetails {
@@ -19,4 +17,38 @@ export interface BookmarkDetails {
   pageNo: number
   memo: string
   wrotenAt: number
+}
+
+export interface BookSearchQuery {
+  q: string
+  intitle?: string
+  inauthor?: string
+  isbn?: string
+}
+
+interface RawBookData {
+  kind: string,
+  id: string,
+  etag: string,
+  selfLink: string,
+  volumeInfo: {
+    title: string,
+    authors: Array<string>,
+    publisher: string,
+    publishedDate: string,
+    description: string,
+    industryIdentifiers: Array<{ type: string, identifier: string}>,
+    pageCount: number,
+    imageLinks: {
+      smallThumbnail: string,
+      thumbnail: string
+    },
+    language: string,
+  }
+}
+
+export interface BookSearchResult {
+  kind: string
+  totalItem: number
+  items: RawBookData[]
 }
--- src/pages/api/books/index.ts
+++ src/pages/api/books/index.ts
@@ -1,36 +1,14 @@
 import fetchWrapper from 'utils/fetchWrapper'
-import { BookDetails } from 'interfaces'
-import getConfig from 'next/config'
-
-const {
-  publicRuntimeConfig: { API_BASE_URL }
-} = getConfig()
-
-const endPoint = API_BASE_URL + 'books/'
+import { BookSearchQuery, BookSearchResult } from 'interfaces'
+import { GoogleBookSearchEndpoint } from 'constants/api'
+import parseBookSearchQuery from 'utils/parseBookSearchQuery'
 
 /**
- * GET /books
- * get book details
+ * GET /
+ * search book detail by google api
  */
-export const getBooks = async () => {
-  const res: BookDetails[] = await fetchWrapper(endPoint)
-
-  return res
-}
-
-/**
- * POST /books
- * add new book detail
- */
-export const postBook = async (body: BookDetails) => {
-  const res: BookDetails = await fetchWrapper<BookDetails>(
-    endPoint, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(body)
-  })
+export const searchBook = async (query: BookSearchQuery) => {
+  const res: BookSearchResult = await fetchWrapper(GoogleBookSearchEndpoint + parseBookSearchQuery(query))
 
   return res
 }
